aa36bea1ab1b1cff1c4dfda98e36ab855f4d6a3d
aimrite2 <137486127+Aim-uses-Malware@users.noreply.github.com>
2026-07-02 10:36:30 +0500

    Modify AnyKernel script for device configuration
    Updated device names and boot shell variables in AnyKernel script.

--- anykernel.sh
+++ anykernel.sh
@@ -1,30 +1,40 @@
 ### AnyKernel3 Ramdisk Mod Script
-## for Xiaomi SPES (sm8250)
+## osm0sis @ xda-developers
 
+### AnyKernel setup
+# global properties
 properties() { '
-kernel.string=SPES Kernel by AimRite2
+kernel.string=
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=spes
-device.name2=
+device.name1=chime
+device.name2=spes
 supported.versions=
 supported.patchlevels=
-'; }
+supported.vendorpatchlevels=
+'; } # end properties
 
+### AnyKernel install
+## boot files attributes
 boot_attributes() {
 set_perm_recursive 0 0 755 644 $RAMDISK/*;
 set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
-BLOCK=/dev/block/bootdevice/by-name/boot;
-IS_SLOT_DEVICE=1;
+# boot shell variables
+BLOCK=auto;
+IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
 
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
-dump_boot;
-write_boot;
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
